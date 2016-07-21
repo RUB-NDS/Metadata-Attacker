9b7498236c6751b15b2038f319814858f2b64356
Sascha Brendel <info@slashdesign.it>
2016-07-21 23:06:02 +0200

    Created AtomicParsleyFile class.

--- web/video/AtomicParsleyFile.php
+++ web/video/AtomicParsleyFile.php
@@ -1,11 +1,13 @@
 <?php
-namespace Lednerb;
 
 class AtomicParsleyFile
 {
-    protected $filename;
+    const filepath = "/tmp/Metadata-Attacker/";
 
-    // Possible Metadata from AtomicParsley
+    protected $filename;
+    protected $short;
+
+    // Possible Metadata from AtomicParsley.
     protected $artist;
     protected $title;
     protected $album;
@@ -39,8 +41,62 @@ class AtomicParsleyFile
     protected $keyword;
     protected $podcastURL;
 
+    /**
+     * AtomicParsleyFile constructor.
+     *
+     * @param bool $short for the 30 sec video. Otherwise the 5 min video would be used.
+     */
+    public function __construct($short = true)
+    {
+        $this->short = $short;
+        $this->filename = md5(microtime()) . ".mp4";
+    }
 
-  // Setters
+    /**
+     * Saves the configured dummy video in the $filepath folder with the Metadata.
+     *
+     * @return bool
+     */
+    public function save() {
+        if(! file_exists(AtomicParsleyFile::filepath))
+            mkdir(AtomicParsleyFile::filepath);
+
+        if(file_put_contents(AtomicParsleyFile::filepath . $this->filename, $this->getTestfile()) !== false)
+            return true;
+
+        return false;
+    }
+
+    /**
+     * Returnes the correct video dummy file for the selected size.
+     *
+     * @return string
+     */
+    public function getTestfile() {
+        if($this->isShort())
+            return file_get_contents("web/video/30sec.mp4");
+
+        return file_get_contents("web/video/5min.mp4");
+    }
+
+    /**
+     * Returns the full filepath to the temporary file.
+     *
+     * @return string
+     */
+    public function getFullFilepath() {
+        return AtomicParsleyFile::filepath . $this->filename;
+    }
+
+    // Simple Setters
+    /**
+     * @param mixed $filename
+     */
+    public function setFilename($filename)
+    {
+        $this->filename = $filename;
+    }
+
     /**
      * @param mixed $artist
      */
@@ -297,6 +353,277 @@ class AtomicParsleyFile
         $this->podcastURL = $podcastURL;
     }
 
+    // Simple Getters
+    /**
+     * @return mixed
+     */
+    public function getFilename()
+    {
+        return $this->filename;
+    }
 
+    /**
+     * @return mixed
+     */
+    public function getArtist()
+    {
+        return $this->artist;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAlbum()
+    {
+        return $this->album;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGenre()
+    {
+        return $this->genre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTracknum()
+    {
+        return $this->tracknum;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDisk()
+    {
+        return $this->disk;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLyrics()
+    {
+        return $this->lyrics;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLyricsFile()
+    {
+        return $this->lyricsFile;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getComposer()
+    {
+        return $this->composer;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCopyright()
+    {
+        return $this->copyright;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGrouping()
+    {
+        return $this->grouping;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getArtwork()
+    {
+        return $this->artwork;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBpm()
+    {
+        return $this->bpm;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAlbumArtist()
+    {
+        return $this->albumArtist;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCompilation()
+    {
+        return $this->compilation;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHdvideo()
+    {
+        return $this->hdvideo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdvisory()
+    {
+        return $this->advisory;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStik()
+    {
+        return $this->stik;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLongdesc()
+    {
+        return $this->longdesc;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStoredesc()
+    {
+        return $this->storedesc;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTVNetwork()
+    {
+        return $this->TVNetwork;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTVShowName()
+    {
+        return $this->TVShowName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTVEpisode()
+    {
+        return $this->TVEpisode;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTVSeasonNum()
+    {
+        return $this->TVSeasonNum;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTVEpisodeNum()
+    {
+        return $this->TVEpisodeNum;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPodcastFlag()
+    {
+        return $this->podcastFlag;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getKeyword()
+    {
+        return $this->keyword;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPodcastURL()
+    {
+        return $this->podcastURL;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isShort()
+    {
+        return $this->short;
+    }
 
 }
