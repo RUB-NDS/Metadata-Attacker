00557cab63ff3c87a9e74cea3b939e1e7645108a
Sascha Brendel <info@slashdesign.it>
2016-08-18 10:26:05 +0200

    Improved docker installation. Composer works out of the box.

---
+++ docker/php-fpm/get-composer.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# This script comes from https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
+
+EXPECTED_SIGNATURE=$(wget https://composer.github.io/installer.sig -O - -q)
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+
+if [ "$EXPECTED_SIGNATURE" = "$ACTUAL_SIGNATURE" ]
+then
+    php composer-setup.php --quiet
+    RESULT=$?
+    rm composer-setup.php
+    exit $RESULT
+else
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
